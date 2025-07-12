8d9ca2e982e91e74b8a12028a0311751f002ef7f
POKEMONFAN4 <garchompsuper@gmail.com>
2025-07-12 22:59:59 +0530

    Create missing main.tsx file

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Dark Theme Anime Streaming Website</h1>
+    </div>
+  </React.StrictMode>,
+)
